1b35e054c0ac1dd36304563254b166064cf8dc97
Виктор <eee@mail.ru>
2024-01-07 21:29:44 +0300

    yes

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+
+        for (Integer x : sourceList) {
+            if (x % 2 == 0) {
+                list.addLast(x);
+            } else {
+                list.addFirst(x);
+            }
+        }
+
+        return list;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,21 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int fun1 = (int) (Math.pow(Integer.parseInt(a), 2) * 5 + 3);
+        int fun2 = (int) (Math.pow(Integer.parseInt(b), 2) * 5 + 3);
+        if (fun1 != fun2) return fun1 - fun2;
+        else return Integer.parseInt(a) - Integer.parseInt(b);
     }
 }
